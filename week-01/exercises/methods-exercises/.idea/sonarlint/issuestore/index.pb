4d64bf04c6774b13dfa431b20879de5c84f60863
Brian <brian.hacker@ire.minnate.edu>
2022-02-10 18:09:40 -0600

    removed lint from git, completed Exercise 14

--- week-01/exercises/methods-exercises/.idea/sonarlint/issuestore/index.pb
+++ week-01/exercises/methods-exercises/.idea/sonarlint/issuestore/index.pb
@@ -9,5 +9,3 @@ C
 src/Exercise12.java,c\6\c64115f24e26b9c80e2fe1c67aaa6c845e5f67ea
 C
 src/Exercise13.java,e\e\eec32ec7c6fed29c350f09f138fe04195de97c39
-C
-src/Exercise14.java,a\b\ab4ab64bed0c412daca3691ba277b4b7df84eadd
--- week-01/exercises/methods-exercises/src/Exercise14.java
+++ week-01/exercises/methods-exercises/src/Exercise14.java
@@ -1,3 +1,6 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Exercise14 {
     /* SHORT SURVEY
 
@@ -12,4 +15,62 @@ public class Exercise14 {
 
     Use methods to break the program into reusable blocks of code.
      */
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("What is your first name?");
+        String firstName = getString(console);
+        System.out.println("What is your last name?");
+        String lastName = getString(console);
+        System.out.println("How many towns/cities have you lived in?");
+        int cities = getInt(console);
+        System.out.println("How many musical instruments can you play?");
+        int instruments = getInt(console);
+
+        System.out.printf("%n%nResults:%n%nName: %s %s%nCities lived in: %s%nInstrument skills: %s%n", firstName, lastName, cities, instruments);
+
+    }
+
+    public static int getInt(Scanner console) {
+        int input = 0;
+        boolean isInt = false;
+        do {
+            try {
+                System.out.println("Enter an Integer:");
+                input = Integer.parseInt(console.nextLine());
+                isInt = true;
+            } catch (NumberFormatException e) {
+                System.out.println("That's not an Integer");
+                isInt = false;
+            } catch (NoSuchElementException e) {
+                System.out.printf("Scanner error: no element found%nNoSuchElementException%n");
+                isInt = false;
+            } catch (IllegalStateException e) {
+                System.out.printf("Scanner error: scanner closed%IllegalStateException%n");
+                isInt = false;
+            }
+        } while (!isInt);
+        return input;
+    }
+    public static String getString(Scanner console) {
+        String input = "";
+        boolean isWord = false;
+        do {
+            try {
+                System.out.println("Enter a word:");
+                input = console.nextLine();
+                isWord = input.matches("[a-zA-Z]+");
+                if (!isWord) {
+                    System.out.println("That's not a word");
+                };
+            } catch (NoSuchElementException e) {
+                System.out.printf("Scanner error: probably no line element found%nNoSuchElementException%n");
+                isWord = false;
+            } catch (IllegalStateException e) {
+                System.out.printf("Scanner error: scanner probably closed%IllegalStateException%n");
+                isWord = false;
+            }
+        } while (!isWord);
+        return input;
+    }
 }
